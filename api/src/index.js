06cccd6a9d104f52fe25c95df1f36b108f89c280
Bayram Erdem <bayram9erdem@gmail.com>
2025-05-17 13:33:42 +0200

    meal & reservations end points

--- api/src/index.js
+++ api/src/index.js
@@ -14,10 +14,10 @@ const apiRouter = express.Router();
 // You can delete this route once you add your own routes
 apiRouter.get("/", async (req, res) => {
   const users = await knex.raw("select * from hyfparticipants");
-  res.send(users[0])
+  res.send(users[0]);
 });
 
-  // This nested router example can also be replaced with your own sub-router
+// This nested router example can also be replaced with your own sub-router
 apiRouter.use("/nested", nestedRouter);
 
 app.use("/api", apiRouter);
@@ -65,3 +65,167 @@ apiRouter.get("/last-meal", async (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
+
+// MEALS
+
+// Search - meals by name
+// /meals?name=dessert
+// /meals?name=chicken
+// query params
+// /meals?name=chickenxffdate=2023-10-01
+apiRouter.get("/search-meals", async (req, res) => {
+  // /meals?name=dessert
+  console.log(req.query);
+  const name = req.query.name;
+  const mealQuery = `SELECT * FROM meal WHERE title LIKE '%${name}%'`;
+  // const mealQuery = "SELECT * FROM meal WHERE name LIKE '%" + name + "%'";
+  const [meal] = await knex.raw(mealQuery);
+  res.json(meal);
+});
+
+// 1. Route to get all meals
+apiRouter.get("/meals", async (req, res) => {
+  const mealQuery = `SELECT * FROM meal`;
+  const [meal] = await knex.raw(mealQuery);
+  res.json(meal);
+});
+
+// 2. Route to add a new meal (POST)
+apiRouter.post("/meals", async (req, res) => {
+  // Extract the title from the request body
+  const title = req.body.title;
+
+  // Insert the new meal into the database using a raw SQL query with direct string interpolation
+  const insertQuery = `INSERT INTO meal (title) VALUES ('${title}')`;
+  await knex.raw(insertQuery);
+
+  // Retrieve the newly added meal from the database by its title
+  const selectQuery = `SELECT * FROM meal WHERE title = '${title}'`;
+  const [newMeal] = await knex.raw(selectQuery);
+
+  // Send the newly added meal as a response
+  res.status(201).json(newMeal[0]);
+});
+
+// 3. Route to get a meal by ID
+// /meals/1
+// /meals/2
+// path params
+
+apiRouter.get("/meals/:id", async (req, res) => {
+  const mealId = req.params.id;
+  const mealQuery = `SELECT * FROM meal WHERE id = ${mealId}`;
+  const mealQuery2 = "SELECT * FROM meal WHERE id = " + mealId + ";";
+
+  const [meal] = await knex.raw(mealQuery);
+  res.json(meal);
+});
+
+// 4. Route to update a meal by ID
+apiRouter.put("/meals/:id", async (req, res) => {
+  const mealId = req.params.id;
+  const { title } = req.body;
+
+  // Construct the SQL query to update the meal
+  const updateQuery = `UPDATE meal SET title = '${title}' WHERE id = ${mealId}`;
+
+  // Execute the update query
+  await knex.raw(updateQuery);
+
+  // Retrieve the updated meal from the database
+  const selectQuery = `SELECT * FROM meal WHERE id = ${mealId}`;
+  const [updatedMeal] = await knex.raw(selectQuery);
+
+  // Send the updated meal as a response
+  res.json(updatedMeal[0]);
+});
+
+// 5. Route to delete a meal by ID
+apiRouter.delete("/meals/:id", async (req, res) => {
+  const mealId = req.params.id;
+
+  // Construct the SQL query to delete the meal
+  const deleteQuery = `DELETE FROM meal WHERE id = ${mealId}`;
+
+  // Execute the delete query
+  await knex.raw(deleteQuery);
+
+  // Send a success response
+  res.status(204).send(); // 204 No Content is a common response for successful DELETE requests
+});
+
+// RESERVATIONS
+
+// 1. Route to get all reservations
+apiRouter.get("/reservations", async (req, res) => {
+  const reservationQuery = `SELECT * FROM reservation`;
+  const reservation = await knex.raw(reservationQuery);
+  res.json(reservation[0]);
+});
+
+// 2. Route to add a new reservation (POST) ?
+apiRouter.post("/reservations", async (req, res) => {
+  // Extract the title from the request body
+  const { meal_id } = req.body;
+
+  // Insert the new reservation into the database using a raw SQL query with direct string interpolation
+  const insertQuery = `INSERT INTO reservation (meal_id) VALUES (${meal_id})`;
+  await knex.raw(insertQuery);
+
+  // Retrieve the newly added reservation from the database by its ID
+  const selectQuery = `SELECT * FROM reservation WHERE meal_id = ${meal_id}`;
+  const newReservation = await knex.raw(selectQuery);
+
+  // Send the newly added reservation as a response
+  res.status(201).json(newReservation[0]);
+});
+
+// 3. Route to get a reservation by ID
+// /reservation/1
+// /reservation/2
+// path params
+
+apiRouter.get("/reservations/:id", async (req, res) => {
+  const reservationId = req.params.id;
+  const reservationQuery = `SELECT * FROM reservation WHERE id = ${reservationId}`;
+  const reservationQuery2 =
+    "SELECT * FROM reservation WHERE id = " + reservationId + ";";
+
+  const reservation = await knex.raw(reservationQuery);
+  res.json(reservation[0]);
+});
+
+// 4. Route to update a reservation by ID
+apiRouter.put("/reservations/:id", async (req, res) => {
+  const reservationId = req.params.id;
+  const email = req.body.bayram;
+
+  // Construct the SQL query to update the meal
+  const updateQuery = `UPDATE reservation SET contact_email = '${email}' WHERE id = ${reservationId}`;
+
+  // Execute the update query
+  await knex.raw(updateQuery);
+
+  // Retrieve the updated meal from the database
+  const selectQuery = `SELECT * FROM reservation WHERE id = ${reservationId}`;
+  const updatedReservation = await knex.raw(selectQuery);
+
+  // Send the updated meal as a response
+  res.json(updatedReservation[0]);
+});
+
+// 5. Route to delete a reservation by ID
+// /api/reservations/:id	DELETE	Deletes the reservation by id
+
+apiRouter.delete("/reservations/:id", async (req, res) => {
+  const reservationId = req.params.id;
+  // Construct the SQL query to delete the meal
+  const deleteQuery = `DELETE FROM reservation WHERE id = ${reservationId}`;
+  // Execute the delete query
+  await knex.raw(deleteQuery);
+  // Send a success response
+  res.status(204).send(); // 204 No Content is a common response for successful DELETE requests
+});
+
+// Update ile ilgili 3 tane
+// Put post get
