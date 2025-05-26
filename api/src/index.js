8ada02caf7d270b3ac53a303acfc71f2decb2084
Bayram Erdem <bayram9erdem@gmail.com>
2025-05-26 11:29:32 +0200

    Refactor reviews doc

--- api/src/index.js
+++ api/src/index.js
@@ -2,9 +2,9 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import knex from "./database_client.js";
 import mealsRouter from "./routers/meals.js";
 import reservationsRouter from "./routers/reservations.js";
+import reviewsRouter from "./routers/reviews.js";
 
 const app = express();
 app.use(cors());
@@ -14,80 +14,11 @@ const apiRouter = express.Router();
 
 apiRouter.use("/meals", mealsRouter);
 apiRouter.use("/reservations", reservationsRouter);
+apiRouter.use("/reviews", reviewsRouter);
 
 app.use("/api", apiRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
-});
-
-// REVIEWS
-
-// ADD a NEW REVIEW TO THE DATABASE
-apiRouter.post("/reviews", async (req, res) => {
-  const stars = req.body.stars;
-  const mealId = req.body.meal_id;
-
-// Here I'm inserting a new review qith only the stars field
-  const insertQuery = `INSERT INTO Review (stars, meal_id) VALUES (${stars}, ${mealId})`;
-await knex.raw(insertQuery);
-
-//Then I get this inserted review
-const selectQuery = `SELECT * FROM Review ORDER BY id DESC LIMIT 1;`;
-const newReview = await knex.raw(selectQuery);
-res.json(newReview[0]);
-});
-
-
-
-// UPDATE A REVIEW
-apiRouter.put("/reviews/:id", async (req, res) => {
-  const reviewId = req.params.id;
-  const stars = req.body.stars;
-
-  // Here'sQL query to update the review
-  const updateQuery = `UPDATE Review SET stars = ${stars} WHERE id = ${reviewId}`;
-
-  // We make the updated query run down here
-  await knex.raw(updateQuery);
-
-  // We're fetching updated review here
-  const selectQuery = `SELECT * FROM Review WHERE id = ${reviewId}`;
-  const updatedReview = await knex.raw(selectQuery);
-
-  // Send the updated review as a response
-  res.json(updatedReview[0]);
-});
-
-
-// DELETE A REVIEW
-apiRouter.delete("/reviews/:id", async (req, res) => {
-  const reviewId = req.params.id;
-
-  // The SQL to delete the review
-  const deleteQuery = `DELETE FROM Review WHERE id = ${reviewId}`;
-
-  // We run the delete query
- await knex.raw(deleteQuery);
-
-// Send a success response to let the user know the review was deleted
-  res.status(204).send(); // 204 No Content is a common response for successful DELETE requests
-});
-
-// GET ALL REVIEWS
-apiRouter.get("/reviews", async (req, res) => {
-  const reviewQuery = `SELECT * FROM Review`;
-  const reviews = await knex.raw(reviewQuery);
-  res.json(reviews[0]);
-});
-
-// GET A REVIEW BY ID
-apiRouter.get("/reviews/:id", async (req, res) => {
-  const reviewId = req.params.id;
-
-  const reviewQuery = `SELECT * FROM Review WHERE id = ${reviewId}`;
-  const review = await knex.raw(reviewQuery);
-  res.json(review[0]);
-
 });
 
