f574f1a38cb81cfe7cd2330c94d8dc9543355a78
Bayram Erdem <bayram9erdem@gmail.com>
2025-06-21 19:47:46 +0200

    Added intro to the HomePage component

--- app-next/components/HomePage/HomePage.jsx
+++ app-next/components/HomePage/HomePage.jsx
@@ -4,9 +4,19 @@ import "@/components/MealsList/MealsList.css";
 
 function HomePage() {
   return (
-    <>
+    <div className="homepage">
+      <h1>Welcome to our Meal Sharing page</h1>
+      <p className="intro">
+        We’re glad you stopped by! 👋  
+        Just like in a Danish kitchen, we believe food tastes better when it's shared.  
+        Below you’ll find some of our favorite meals — made with love and care.  
+        Choose something that makes you happy, and enjoy every bite.  
+        <br /><br />
+        Velbekomme! 🍽️
+      </p>
+
       <MealsList />
-    </>
+    </div>
   );
 }
 
