b7d24052dff8ae2d93a82ffd693e87e80f5e55d0
Bayram Erdem <bayram9erdem@gmail.com>
2025-06-30 21:48:23 +0200

    Updated MealsPage to display meals in a table format

--- app-next/app/meals/page.jsx
+++ app-next/app/meals/page.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import {
-    Card,
-    CardContent,
-    CardMedia,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
     Typography,
-    Grid,
-    CardActionArea,
     Box,
+    Avatar,
 } from "@mui/material";
 
-// Mark as server component by removing useEffect/useState and using async fetch
 export default async function MealsPage() {
     const res = await fetch("http://localhost:3001/api/meals", { cache: "no-store" });
     const meals = await res.json();
@@ -20,32 +22,28 @@ export default async function MealsPage() {
             <Typography variant="h4" gutterBottom>
                 All Meals
             </Typography>
-            <Grid container spacing={3}>
-                {meals.map((meal) => (
-                    <Grid item xs={12} sm={6} md={4} key={meal.id}>
-                        <Link href={`/meals/${meal.id}`} passHref legacyBehavior>
-                            <CardActionArea component="a">
-                                <Card>
-                                    {meal.image_url && (
-                                        <CardMedia
-                                            component="img"
-                                            height="180"
-                                            image={meal.image_url}
-                                            alt={meal.title}
-                                        />
-                                    )}
-                                    <CardContent>
-                                        <Typography variant="h6">{meal.title}</Typography>
-                                        <Typography variant="body2" color="text.secondary">
-                                            {meal.description}
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </CardActionArea>
-                        </Link>
-                    </Grid>
-                ))}
-            </Grid>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Title</TableCell>
+                            <TableCell>Description</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {meals.map((meal) => (
+                            <TableRow key={meal.id} hover>
+                                <TableCell>
+                                    <Link href={`/meals/${meal.id}`} style={{ textDecoration: "none", color: "inherit" }}>
+                                        {meal.title}
+                                    </Link>
+                                </TableCell>
+                                <TableCell>{meal.description}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Box>
     );
 }
